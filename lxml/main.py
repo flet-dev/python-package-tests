1ac3ad20d88753ea01413829382077cb2e8df2c0
InesaFitsner <inesa@appveyor.com>
2024-11-25 10:54:11 -0800

    lxml texts

---
+++ lxml/main.py
@@ -0,0 +1,51 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Flet libs test"
+
+    def parseXML(xmlFile):
+        """
+        Parse the xml
+        """
+        from lxml import etree
+
+        with open(xmlFile) as fobj:
+            xml = fobj.read()
+
+            root = etree.fromstring(xml)
+
+        for appt in root.getchildren():
+            for elem in appt.getchildren():
+                if not elem.text:
+                    text = "None"
+                else:
+                    text = elem.text
+                print(elem.tag + " => " + text)
+
+    def lxml_tests(e):
+
+        def test_basic():
+
+            try:
+
+                parseXML("example.xml")
+
+                page.add(ft.Text("lxml: test_basic - OK"))
+            except Exception as e:
+                page.add(ft.Text(f"lxml: test_basic - error: {e}", selectable=True))
+
+        test_basic()
+
+    page.add(
+        ft.SafeArea(
+            ft.Column(
+                [
+                    ft.ElevatedButton("Run lxml tests", on_click=lxml_tests),
+                ]
+            )
+        )
+    )
+
+
+ft.app(main)
