7e750eec5cde11b5bc114945081faf9737225ce2
InesaFitsner <inesa@appveyor.com>
2024-11-29 13:28:24 -0800

    geopandas test

--- geopandas/main.py
+++ geopandas/main.py
@@ -11,7 +11,45 @@ def main(page: ft.Page):
 
         def test_basic():
 
+            # https://geopandas.org/en/stable/gallery/create_geopandas_from_pandas.html
+
             try:
+
+                # from geodatasets import get_path
+
+                import geopandas
+
+                # import matplotlib.pyplot as plt
+                import pandas as pd
+
+                df = pd.DataFrame(
+                    {
+                        "City": [
+                            "Buenos Aires",
+                            "Brasilia",
+                            "Santiago",
+                            "Bogota",
+                            "Caracas",
+                        ],
+                        "Country": [
+                            "Argentina",
+                            "Brazil",
+                            "Chile",
+                            "Colombia",
+                            "Venezuela",
+                        ],
+                        "Latitude": [-34.58, -15.78, -33.45, 4.60, 10.48],
+                        "Longitude": [-58.66, -47.91, -70.66, -74.08, -66.86],
+                    }
+                )
+
+                gdf = geopandas.GeoDataFrame(
+                    df,
+                    geometry=geopandas.points_from_xy(df.Longitude, df.Latitude),
+                    crs="EPSG:4326",
+                )
+
+                print(gdf.head())
 
                 page.add(ft.Text("geopandas: test_basic - OK"))
             except Exception as e:
